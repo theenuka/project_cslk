adb1eb92faee28ad835b14f54aaaa04b9b292e37
theenuka <theenukabandara@gmail.com>
2025-09-19 20:15:06 +0530

    created class for birds

--- sample.java
+++ sample.java
@@ -3,3 +3,9 @@ class Animal {
         System.out.println("Some generic animal sound");
     }
 }
+class Bird extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Chirp Chirp");
+    }
+}
